016cf77d5988cbeede310d8f4d026721863849b6
Diego Melgarejo San Martin <dimel@microsoft.com>
2020-12-15 23:10:43 -0300

    init

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <H1>Home</H1>
+            </div>
+        );
+    }
+}
+
+export default Home;
